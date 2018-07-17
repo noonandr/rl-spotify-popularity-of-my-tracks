00488044516e827511299876c5824aad688db48c
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-07-17 16:34:20 +0100

    chore(vue): RL-002 removed and renamed default Vue generated files for project

---
+++ src/views/Spotify.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spotify">
+    <Tracks msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Tracks from '@/components/Tracks.vue'
+
+export default {
+  name: 'spotify',
+  components: {
+    Tracks
+  }
+}
+</script>
